4720732ecd935bc16661f90a41fc4e6e5ec8cb00
Piotr <piotrku91@github.com>
2021-09-05 15:09:36 +0200

    universal template function

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -1,15 +1,14 @@
 #pragma once
 #include <functional>
-#include <string>
 #include <thread>
 #include <type_traits>
 
 
 template <typename T, typename... T2>
-void schedule(T func, std::chrono::seconds dur, T2... Args)
+void schedule(T functionLambda, std::chrono::seconds dur, T2... Args)
 {
     std::this_thread::sleep_for(dur);
-    if (std::is_function<decltype(func)>::value) {func(Args...);};
+    std::invoke(functionLambda,Args...);
 }
 
 
