c4397683d828620da00dd575fb6056a609454694
Piotr Rogalski <progalski89@gmail.com>
2021-09-07 01:48:44 +0200

    Fix

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
+#include <chrono>
 #include <thread>
 
-template<typename F, typename ...Args>
+template <typename F, typename... Args>
 void schedule(F&& func, const std::chrono::seconds time, const Args&... args) {
     std::this_thread::sleep_for(time);
     func(args...);
