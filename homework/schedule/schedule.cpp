3d34019fc16c3b375a28316c65bd48220f2432d8
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-09-10 12:32:29 +0200

    Adding overloading of function schedule

--- homework/schedule/schedule.cpp
+++ homework/schedule/schedule.cpp
@@ -6,3 +6,13 @@ void schedule (std::function<void()> func,const std::chrono::seconds duration) {
     std::this_thread::sleep_for(duration);
     func();
 }
+
+void schedule (std::function<void(int)> func,const std::chrono::seconds duration, const int value){
+    std::this_thread::sleep_for(duration);
+    func(value);
+}
+
+void schedule (std::function<void(std::string, double)> func,const std::chrono::seconds duration,  std::string text, double number) {
+    std::this_thread::sleep_for(duration);
+    func(text, number);
+}
--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <functional>
 #include <chrono>
+#include <string>
 #include <thread>
 
 void schedule (std::function<void()> func,const std::chrono::seconds duration);
+void schedule (std::function<void(int)> func,const std::chrono::seconds duration,  int value);
+void schedule (std::function<void(std::string, double)> func,const std::chrono::seconds duration,  std::string text, double number);
