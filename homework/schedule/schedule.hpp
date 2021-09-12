42e3b9a901e584a72fb2a7dd1426f0f61b882ee5
aniolm <maciej.aniol.programista@gmail.comgit config --global user.name Your>
2021-09-12 22:38:28 +0300

    Template function complete

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -1,29 +1,34 @@
 #ifndef SCHEDULE_HPP
 #define SCHEDULE_HPP
 #include <chrono>
-#include <thread>
 #include <functional>
+#include <thread>
+
+template <typename T, typename... Args>
+void schedule(T func, struct std::chrono::duration<int64_t> duration, Args... args) {
+    std::this_thread::sleep_for(duration);
+    func(args...);
+}
 
 void schedule(std::function<void()> func,
-              struct std::chrono::duration<int64_t> duration){
-    std::this_thread::sleep_for (duration);
+              struct std::chrono::duration<int64_t> duration) {
+    std::this_thread::sleep_for(duration);
     func();
 }
 
 void schedule(std::function<void(int parameter)> func,
-            struct std::chrono::duration<int64_t> duration, 
-            int parameter){
-    std::this_thread::sleep_for (duration);
+              struct std::chrono::duration<int64_t> duration,
+              int parameter) {
+    std::this_thread::sleep_for(duration);
     func(parameter);
 }
 
 void schedule(std::function<void(std::string, double)> func,
-                struct std::chrono::duration<int64_t> duration, 
-                std::string text, 
-                double number){    
-    std::this_thread::sleep_for (duration);
-    func(text,number);
-    
+              struct std::chrono::duration<int64_t> duration,
+              std::string text,
+              double number) {
+    std::this_thread::sleep_for(duration);
+    func(text, number);
 }
 
 #endif
