103b5023fdc267b046714801af90ed4f6445b53b
kondorski <thegreatvaultofknowledge@gmail.com>
2021-09-15 00:05:59 +0200

    working basic tests

--- homework/schedule/schedule.cpp
+++ homework/schedule/schedule.cpp
@@ -1,18 +1,17 @@
 #include "schedule.hpp"
 #include <thread>
 
-
-void schedule( void(*func)(), std::chrono::duration<int, std::ratio<1L, 1L>> duration) {
+void schedule( std::function<void()> func, const std::chrono::duration<int, std::ratio<1L, 1L>> duration) {
     std::this_thread::sleep_until(std::chrono::system_clock::now() + duration);
-    (func)();
+    func();
 }
 
-void schedule( void(*func)(int a), std::chrono::duration<int, std::ratio<1L, 1L>> duration, int a) {
+void schedule( std::function<void(int a)> func, const std::chrono::duration<int, std::ratio<1L, 1L>> duration, int & a) {
     std::this_thread::sleep_until(std::chrono::system_clock::now() + duration);
-    (func)(a);
+    func(a);
 }
 
-void schedule( void(*func)(std::string s, double d), std::chrono::duration<int, std::ratio<1L, 1L>> duration, std::string s, double d) {
+void schedule( std::function<void(std::string s, double d)> func, const std::chrono::duration<int, std::ratio<1L, 1L>> duration, std::string & s, double & d) {
     std::this_thread::sleep_until(std::chrono::system_clock::now() + duration);
-    (func)(s, d);
+    func(s, d);
 }
--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -1,7 +1,8 @@
 #pragma once
 #include <chrono>
 #include <string>
+#include <functional>
 
-void schedule( void(*func)(), std::chrono::duration<int, std::ratio<1L, 1L>> duration);
-void schedule( void(*func)(int a), std::chrono::duration<int, std::ratio<1L, 1L>> duration, int);
-void schedule( void(*func)(std::string s, double), std::chrono::duration<int, std::ratio<1L, 1L>> duration, std::string s, double d);
+void schedule( std::function<void()> func, const std::chrono::duration<int, std::ratio<1L, 1L>> duration);
+void schedule( std::function<void(int a)> func, const std::chrono::duration<int, std::ratio<1L, 1L>> duration, int&);
+void schedule( std::function<void(std::string s, double d)> func, const std::chrono::duration<int, std::ratio<1L, 1L>> duration, std::string & s, double & d);
