f9ac916c125508d899a0afebcdc7738359035e22
Martyna-Tu <martyna.tucholska.wkr@gmail.com>
2021-09-16 12:11:05 +0200

    template solution

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -4,7 +4,7 @@
 
 
 
-void schedule(std::function<void()> f, std::chrono::seconds time) {
+/*void schedule(void (*f)(), std::chrono::seconds time) {
         
         auto startTimer = std::chrono::system_clock::now();
         auto elapsed_time = [startTimer]() {
@@ -18,7 +18,7 @@ void schedule(std::function<void()> f, std::chrono::seconds time) {
 }
 
 
-void schedule(std::function<void(int i)> f, std::chrono::seconds time, int i) {
+void schedule(void (*f)(int), std::chrono::seconds time, int i) {
 
         auto startTimer = std::chrono::system_clock::now();
         auto elapsed_time = [startTimer]() {
@@ -30,7 +30,7 @@ void schedule(std::function<void(int i)> f, std::chrono::seconds time, int i) {
         }
         f(i);
 }
-void schedule(std::function<void(std::string str, double d)> f, std::chrono::seconds time, std::string str, double d) {
+void schedule(void (*f)(std::string str, double d), std::chrono::seconds time, std::string str, double d) {
 
         auto startTimer = std::chrono::system_clock::now();
         auto elapsed_time = [startTimer]() {
@@ -41,5 +41,20 @@ void schedule(std::function<void(std::string str, double d)> f, std::chrono::sec
         while (elapsed_time() < time.count()) {
         }
         f(str, d);
+}
+/**/
+
+template <typename Fun, typename... Args>
+void schedule(Fun&& fun,  std::chrono::duration<double> time, Args&&...args) {
+
+        auto startTimer = std::chrono::system_clock::now();
+        auto elapsed_time = [startTimer]() {
+                std::chrono::duration<double> diffrence = std::chrono::system_clock::now() - startTimer;
+                return diffrence.count();
+        };
+
+        while (elapsed_time() < time.count()) {
+        }
+        fun(std::forward<Args>(args)...);
 }
 
