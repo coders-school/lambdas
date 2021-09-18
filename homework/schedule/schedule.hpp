6672e2c402ee1088f18577209a6eacd3ac768284
NikczemnyOkularnik <grzegorzbrodziakgd@gmail.com>
2021-09-18 08:42:57 +0200

    added schedule.hpp, schedule 1st task done

---
+++ homework/schedule/schedule.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <functional>
+void schedule(std::function<void()> blankFunc, std::chrono::seconds seconds)
+{   
+    auto demanded_time = seconds.count();
+    auto clock = std::chrono::system_clock::now();
+    auto duration_time = [clock](){
+        std::chrono::duration<double> time_elapsed = std::chrono::system_clock::now() - clock;
+        return time_elapsed.count();
+    };
+    while(duration_time() < demanded_time){}
+}
