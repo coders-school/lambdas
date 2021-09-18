7b7f9f209f9f0e8a856c31b8ed8182aabc28485b
NikczemnyOkularnik <grzegorzbrodziakgd@gmail.com>
2021-09-18 08:56:26 +0200

    schedule 3rd task done

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -9,6 +9,7 @@ void schedule(std::function<void()> blankFunc, std::chrono::seconds seconds)
         return time_elapsed.count();
     };
     while(duration_time() < demanded_time){}
+    blankFunc();
 }
 void schedule(std::function<void(int i)> Func, std::chrono::seconds seconds, int i)
 {   
@@ -19,4 +20,16 @@ void schedule(std::function<void(int i)> Func, std::chrono::seconds seconds, int
         return time_elapsed.count();
     };
     while(duration_time() < demanded_time){}
+    Func(i);
+}
+void schedule(std::function<void(std::string s, double d)> Func, std::chrono::seconds seconds, std::string s, double d)
+{   
+    auto demanded_time = seconds.count();
+    auto clock = std::chrono::system_clock::now();
+    auto duration_time = [clock](){
+        std::chrono::duration<double> time_elapsed = std::chrono::system_clock::now() - clock;
+        return time_elapsed.count();
+    };
+    while(duration_time() < demanded_time){}
+    Func(s, d);
 }
