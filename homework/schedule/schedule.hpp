395781478c87a1c6d9e28498a25d032c93ed584e
Jakub Kaczmarski <kuba.kaczmarski312@gmail.com>
2021-09-19 22:56:46 +0200

    added schedule.cpp to compile options

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -1,10 +1,12 @@
 #pragma once
 #include <chrono>
-#include <string>
 #include <functional>
+#include <iomanip>
+#include <string>
 #include <thread>
+#include <iostream>
 void schedule(std::function<void()> func, std::chrono::seconds duration);
 
-void schedule(std::function<void(int)> func,std::chrono::seconds duration, int param);
+void schedule(std::function<void(int)> func, std::chrono::seconds duration, int param);
 
 void schedule(std::function<void(std::string, double)> func, std::chrono::seconds duration, std::string name, double time);
