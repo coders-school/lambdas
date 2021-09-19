2a9839bd42e76a8885b3f8f5dd09ebe1e816d2af
Katarzyna Strączyńska <katarzyna.a.straczynska@gmail.com>
2021-09-19 09:16:57 +0200

    Adding as template function

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -1,6 +1,40 @@
 #pragma once
 #include <functional>
-void schedule(std::function<void()> blankFunc, std::chrono::seconds seconds)
+// void schedule(std::function<void()> blankFunc, std::chrono::seconds seconds)
+// {   
+//     auto demanded_time = seconds.count();
+//     auto clock = std::chrono::system_clock::now();
+//     auto duration_time = [clock](){
+//         std::chrono::duration<double> time_elapsed = std::chrono::system_clock::now() - clock;
+//         return time_elapsed.count();
+//     };
+//     while(duration_time() < demanded_time){}
+//     blankFunc();
+// }
+// void schedule(std::function<void(int i)> Func, std::chrono::seconds seconds, int i)
+// {   
+//     auto demanded_time = seconds.count();
+//     auto clock = std::chrono::system_clock::now();
+//     auto duration_time = [clock](){
+//         std::chrono::duration<double> time_elapsed = std::chrono::system_clock::now() - clock;
+//         return time_elapsed.count();
+//     };
+//     while(duration_time() < demanded_time){}
+//     Func(i);
+// }
+// void schedule(std::function<void(std::string s, double d)> Func, std::chrono::seconds seconds, std::string s, double d)
+// {   
+//     auto demanded_time = seconds.count();
+//     auto clock = std::chrono::system_clock::now();
+//     auto duration_time = [clock](){
+//         std::chrono::duration<double> time_elapsed = std::chrono::system_clock::now() - clock;
+//         return time_elapsed.count();
+//     };
+//     while(duration_time() < demanded_time){}
+//     Func(s, d);
+// }
+template <typename Func, typename... Args>
+void schedule(Func function, std::chrono::seconds seconds, Args... arguments)
 {   
     auto demanded_time = seconds.count();
     auto clock = std::chrono::system_clock::now();
@@ -9,27 +43,5 @@ void schedule(std::function<void()> blankFunc, std::chrono::seconds seconds)
         return time_elapsed.count();
     };
     while(duration_time() < demanded_time){}
-    blankFunc();
-}
-void schedule(std::function<void(int i)> Func, std::chrono::seconds seconds, int i)
-{   
-    auto demanded_time = seconds.count();
-    auto clock = std::chrono::system_clock::now();
-    auto duration_time = [clock](){
-        std::chrono::duration<double> time_elapsed = std::chrono::system_clock::now() - clock;
-        return time_elapsed.count();
-    };
-    while(duration_time() < demanded_time){}
-    Func(i);
-}
-void schedule(std::function<void(std::string s, double d)> Func, std::chrono::seconds seconds, std::string s, double d)
-{   
-    auto demanded_time = seconds.count();
-    auto clock = std::chrono::system_clock::now();
-    auto duration_time = [clock](){
-        std::chrono::duration<double> time_elapsed = std::chrono::system_clock::now() - clock;
-        return time_elapsed.count();
-    };
-    while(duration_time() < demanded_time){}
-    Func(s, d);
+    function(arguments ...);
 }
