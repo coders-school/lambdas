568d84d69190b4efaf76d7fff7d42a02340d8e09
Jakub Kaczmarski <kuba.kaczmarski312@gmail.com>
2021-09-19 22:58:54 +0200

    Deleted not used semicolons

--- homework/schedule/schedule.cpp
+++ homework/schedule/schedule.cpp
@@ -4,12 +4,12 @@ void schedule(std::function<void()> func, std::chrono::seconds duration) {
     std::this_thread::sleep_for(duration);
 
     func();
-};
+}
 void schedule(std::function<void(int)> func, std::chrono::seconds duration, int param) {
     std::this_thread::sleep_for(duration);
 
     func(param);
-};
+}
 void schedule(std::function<void(std::string s, double d)> func,std::chrono::seconds duration,  std::string name, double time) {
     std::this_thread::sleep_for(duration);
 
