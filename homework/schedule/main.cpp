6f1d6a9324e3486acd3a6a12dc324a57712d595d
Sebastian302653 <82955713+Sebastian302653@users.noreply.github.com>
2021-09-24 21:45:44 +0200

    Update main.cpp

--- homework/schedule/main.cpp
+++ homework/schedule/main.cpp
@@ -2,19 +2,10 @@
 #include <iomanip>
 #include <iostream>
 #include <string>
-#include "schedule.hpp"
 #include <thread>
 #include <functional>
 
-// Napisz funkcję schedule(), która przyjmuje dwa parametry:
-//   Pierwszy parametr to dowolną funkcję func, która nie przyjmuje żadnego argumentu i nic nie zwraca
-//   Drugi parametr to okres czasu duration wyrażony w sekundach (użyj biblioteki std::chrono) Funkcja schedule() ma odczekać podany okres czasu duration, a potem wywołać funkcję func.
-// Napisz również przeciążenie funkcji schedule(), które oprócz okresu czasu ma przyjąć dowolną funkcję, która przyjmuje int i nic nie zwraca. Pomyśl jak przekazać ten argument int do tej funkcji.
-// Napisz kolejne przeciążenie, które będzie przyjmować funkcje, które przyjmują std::string oraz double jako swoje argumenty i nic nie zwracają.
-// Na gałęzi schedule masz już gotowy szkielet do tej aplikacji. Tam dostarcz swój kod.
 
-// Bonus: zaimplementuj schedule jako funkcję szablonową. Wystarczy wtedy tylko jedna jej wersja, bez żadnych przeciążeń.
-// Uwaga, to trudne zadanie na tym etapie kursu!
 void schedule(std::function<void()> func, int duration)
 {
     std::this_thread::sleep_for(std::chrono::seconds(duration));
