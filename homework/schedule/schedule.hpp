2bca9d76d4be79e28a00412ac61d8ceff1a8e85b
Kacu <kacper.kaleta@scythe-studio.com>
2021-09-29 22:01:39 +0200

    completed bonus schedule

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -4,11 +4,8 @@
 #include <functional>
 #include <thread>
 
-void schedule(std::function<void()> func, std::chrono::seconds duration);
-
-void schedule(std::function<void(int)> func, std::chrono::seconds duration, int number);
-
-void schedule(std::function<void(std::string, double)> func, std::chrono::seconds duration, std::string text, double number);
-
-//template<typename Function, typename Duration, typename... Argument>
-//auto schedule(Function Func , Duration duration, Argument ...arg);
+template <typename Func, typename... Args>
+void schedule(Func func, std::chrono::seconds duration, Args... arg) {
+    std::this_thread::sleep_for(duration);
+    func(arg...);
+} 
