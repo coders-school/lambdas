ba65e52f8060e31fe4f112f23aa2ac25979fbb90
Łukasz Meisnerowski <lukasz.meisnerowski@gmail.com>
2021-12-09 22:02:38 +0100

    adding template

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -4,6 +4,11 @@
 #include <thread>
 #include <string>
 
+template<typename Func, typename Type, typename... Variables>
+void schedule(Func f, Type t, Variables... v) {
+    std::this_thread::sleep_for(t);
+    f(std::forward<Variables>(v)...);
+}
 
 void schedule(std::function<void()> , std::chrono::seconds );
 void schedule(std::function<void(int)> , std::chrono::seconds , int );
