1c5c6e2f0b22572fe148b6c65193121bffd7a9c3
Aleksiiej <aleksander.ramut@wp.pl>
2022-09-06 23:23:47 +0200

    secondary task completed

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -2,9 +2,14 @@
 
 #include <chrono>
 #include <functional>
-#include <string>
 #include <thread>
 
 void schedule(std::function<void()> func, std::chrono::seconds duration);
 void schedule(std::function<void(int)> func, std::chrono::seconds duration, int number);
 void schedule(std::function<void(std::string, double)> func, std::chrono::seconds duration, std::string text, double number);
+
+template <typename Function, typename Duration, typename... Arg>
+auto schedule(Function func, Duration duration, Arg... arg) {
+    std::this_thread::sleep_for(duration);
+    func(arg...);
+}
