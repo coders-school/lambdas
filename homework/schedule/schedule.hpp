e0dc4ddea5bef7ea971850ac760b5fa1ee8aaf8b
adrian <7adrian.mail@gmail.com>
2022-10-13 17:55:47 +0200

    fixup! Schedule implementation with template

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -3,8 +3,8 @@
 #include <chrono>
 #include <thread>
 
-template<typename T, typename... Args>
-void schedule(T&& func, const std::chrono::seconds& durationTime, const Args& ... args) {
+template <typename T, typename... Args>
+void schedule(T&& func, const std::chrono::seconds& durationTime, const Args&... args) {
     std::this_thread::sleep_for(durationTime);
     func(args...);
 }
