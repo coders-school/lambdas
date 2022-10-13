88d42774fe29c0e386eaa94313fb53df01523e65
adrian <7adrian.mail@gmail.com>
2022-10-13 17:00:36 +0200

    Schedule implementation for another functions

--- homework/schedule/schedule.cpp
+++ homework/schedule/schedule.cpp
@@ -2,18 +2,54 @@
 
 #include <thread>
 
-void schedule(std::function<void()> func, const duration<long>& durationTime) {
+void schedule(const std::function<void()>& func, const duration<long>& durationTime) {
     std::this_thread::sleep_for(std::chrono::milliseconds(durationTime));
     func();
 }
 
-void schedule(std::function<void(int)> func, const duration<long>& durationTime, int value) {
+void schedule(const std::function<void(int)>& func, const duration<long>& durationTime, int value) {
     std::this_thread::sleep_for(std::chrono::milliseconds(durationTime));
     func(value);
 }
 
-void schedule(std::function<void(const std::string&, double)> func, const duration<long>& durationTime,
+void schedule(const std::function<void(const std::string&, double)>& func, const duration<long>& durationTime,
               const std::string& value, double value2) {
     std::this_thread::sleep_for(std::chrono::milliseconds(durationTime));
     func(value, value2);
+}
+
+void
+schedule(const std::function<void(const std::string&, const std::string&)>& func, const duration<long>& durationTime,
+         const std::string& value, const std::string& value2) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(durationTime));
+    func(value, value2);
+}
+
+void
+schedule(const std::function<void(double, int, int, char)>& func, const duration<long>& durationTime, double value,
+         int value2,
+         int value3, char value4) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(durationTime));
+    func(value, value2, value3, value4);
+}
+
+void schedule(const std::function<void(double, int, int)>& func, const duration<long>& durationTime, double value,
+              int value2,
+              int value3) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(durationTime));
+    func(value, value2, value3);
+}
+
+void
+schedule(const std::function<void(double, int)>& func, const duration<long>& durationTime, double value, int value2) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(durationTime));
+    func(value, value2);
+}
+
+void
+schedule(const std::function<void(char, char, const std::vector<int>&)>& func, const duration<long>& durationTime,
+         char value,
+         char value2, const std::vector<int>& value3) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(durationTime));
+    func(value, value2, value3);
 }
--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -6,9 +6,36 @@
 
 using std::chrono::duration;
 
-void schedule(std::function<void()> func, const duration<long>& durationTime);
+void schedule(const std::function<void()>& func, const duration<long>& durationTime);
 
-void schedule(std::function<void(int)> func, const duration<long>& durationTime, int value);
+void schedule(const std::function<void(int)>& func, const duration<long>& durationTime, int value);
 
-void schedule(std::function<void(const std::string&, double)> func, const duration<long>& durationTime,
+void schedule(const std::function<void(const std::string&, double)>& func, const duration<long>& durationTime,
               const std::string& value, double value2);
+
+void schedule(const std::function<void(const std::string&, const std::string&)>& func,
+              const duration<long>& durationTime,
+              const std::string& value, const std::string& value2);
+
+void schedule(const std::function<void(double, int, int, char)>& func,
+              const duration<long>& durationTime,
+              double value,
+              int value2,
+              int value3,
+              char value4
+);
+
+void schedule(const std::function<void(double, int, int)>& func, const duration<long>& durationTime,
+              double value, int value2, int value3);
+
+void
+schedule(const std::function<void(double, int)>& func, const duration<long>& durationTime, double value, int value2);
+
+void schedule(const std::function<void(char, char, const std::vector<int>&)>& func,
+              const duration<long>& durationTime,
+              char value,
+              char value2,
+              const std::vector<int>& value3
+);
+
+
