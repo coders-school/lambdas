94f8c7207de8b2af35d8319cee779451eef83bf4
adrian <7adrian.mail@gmail.com>
2022-10-13 17:50:48 +0200

    Schedule implementation with template

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -1,41 +1,10 @@
 #pragma once
 
 #include <chrono>
-#include <functional>
-#include <string>
+#include <thread>
 
-using std::chrono::duration;
-
-void schedule(const std::function<void()>& func, const duration<long>& durationTime);
-
-void schedule(const std::function<void(int)>& func, const duration<long>& durationTime, int value);
-
-void schedule(const std::function<void(const std::string&, double)>& func, const duration<long>& durationTime,
-              const std::string& value, double value2);
-
-void schedule(const std::function<void(const std::string&, const std::string&)>& func,
-              const duration<long>& durationTime,
-              const std::string& value, const std::string& value2);
-
-void schedule(const std::function<void(double, int, int, char)>& func,
-              const duration<long>& durationTime,
-              double value,
-              int value2,
-              int value3,
-              char value4
-);
-
-void schedule(const std::function<void(double, int, int)>& func, const duration<long>& durationTime,
-              double value, int value2, int value3);
-
-void
-schedule(const std::function<void(double, int)>& func, const duration<long>& durationTime, double value, int value2);
-
-void schedule(const std::function<void(char, char, const std::vector<int>&)>& func,
-              const duration<long>& durationTime,
-              char value,
-              char value2,
-              const std::vector<int>& value3
-);
-
-
+template<typename T, typename... Args>
+void schedule(T&& func, const std::chrono::seconds& durationTime, const Args& ... args) {
+    std::this_thread::sleep_for(durationTime);
+    func(args...);
+}
