2fb0ea8d048c11ddfdd23db3b1beca12368a625f
Roman Nawrot <romug1337@gmail.com>
2023-01-23 22:27:40 +0100

    little fix

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
+#include <chrono>
 #include <functional>
 #include <iostream>
 #include <string>
-#include <chrono>
 #include <thread>
 
 namespace Schedule {
 
 void schedule(std::function<void()> fptr, std::chrono::seconds duration);
 void schedule(std::function<void(int)> fptr, std::chrono::seconds duration, int i);
-void schedule(std::function<void(std::string, double)> fptr, std::chrono::seconds duration, std::string, double d);
+void schedule(std::function<void(std::string, double)> fptr, std::chrono::seconds duration, std::string s, double d);
 
-}
+}  // namespace Schedule
