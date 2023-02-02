228a4c9ffb91b87f50ed83ae382f4416d2ab3a14
Roman Nawrot <romug1337@gmail.com>
2023-02-02 18:05:08 +0100

    format fix

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -9,11 +9,9 @@
 //namespace Schedule {
 
 template <typename Func, typename... Args>
-void schedule(Func fptr, std::chrono::seconds duration, Args... args)
-{
+void schedule(Func fptr, std::chrono::seconds duration, Args... args) {
     std::this_thread::sleep_for(std::chrono::seconds(duration));
     fptr(args...);
 }
 // void schedule(std::function<void(int)> fptr, std::chrono::seconds duration, int i);
 // void schedule(std::function<void(std::string, double)> fptr, std::chrono::seconds duration, std::string s, double d);
-
