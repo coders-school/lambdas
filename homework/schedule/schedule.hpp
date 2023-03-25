8dfc6813ad988c023f01cd6e2567b68af4aab470
PawelN93 <paweln1993@gmail.com>
2023-03-25 20:44:14 +0100

    schedule bonus task

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -2,33 +2,42 @@
 #include <functional>
 #include <string>
 
-void schedule(std::function<void()>fptr, std::chrono::seconds duration){
+// void schedule(std::function<void()>fptr, std::chrono::seconds duration){
 
+//     auto start = std::chrono::system_clock::now();
+//     while(std::chrono::duration_cast<std::chrono::seconds>(
+//         std::chrono::system_clock::now() - start) <= duration){
+
+//     }
+//     fptr();
+// }
+
+// void schedule(std::function<void(int)>fptr, std::chrono::seconds duration, int a){
+
+//     auto start = std::chrono::system_clock::now();
+//     while(std::chrono::duration_cast<std::chrono::seconds>(
+//         std::chrono::system_clock::now() - start) <= duration){
+
+//     }
+//     fptr(a);
+// }
+
+// void schedule(std::function<void(std::string, double)>fptr, std::chrono::seconds duration,
+//     std::string str, double a){
+
+//     auto start = std::chrono::system_clock::now();
+//     while(std::chrono::duration_cast<std::chrono::seconds>(
+//         std::chrono::system_clock::now() - start) <= duration){
+
+//     }
+//     fptr(str, a);
+// }
+
+template <typename Fun, typename... Args>
+void schedule(Fun fptr, std::chrono::seconds duration, Args... args) {
     auto start = std::chrono::system_clock::now();
-    while(std::chrono::duration_cast<std::chrono::seconds>(
-        std::chrono::system_clock::now() - start) <= duration){
-
+    while (std::chrono::duration_cast<std::chrono::seconds>(
+               std::chrono::system_clock::now() - start) <= duration) {
     }
-    fptr();
-}
-
-void schedule(std::function<void(int)>fptr, std::chrono::seconds duration, int a){
-
-    auto start = std::chrono::system_clock::now();
-    while(std::chrono::duration_cast<std::chrono::seconds>(
-        std::chrono::system_clock::now() - start) <= duration){
-
-    }
-    fptr(a);
-}
-
-void schedule(std::function<void(std::string, double)>fptr, std::chrono::seconds duration,
-    std::string str, double a){
-
-    auto start = std::chrono::system_clock::now();
-    while(std::chrono::duration_cast<std::chrono::seconds>(
-        std::chrono::system_clock::now() - start) <= duration){
-
-    }
-    fptr(str, a);
+    fptr(args...);
 }
