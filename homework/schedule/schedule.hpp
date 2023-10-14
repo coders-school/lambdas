d209e2e9d99fa020c94d67ed1e14a8614b01bc72
Bartosz Rozenkop <bartez33a@gmail.com>
2023-10-14 20:56:41 +0200

    Implemented using variadic template

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -3,27 +3,8 @@
 #include <functional>
 #include <string>
 
-// void schedule(std::function<void(void)> func, std::chrono::seconds duration);
-// void schedule(std::function<void(int)> func, std::chrono::seconds duration, int func_arg);
-// void schedule(std::function<void(std::string, double)> func, std::chrono::seconds duration, std::string text, double value);
-
-void schedule(std::function<void(void)> func, std::chrono::seconds duration) {
-    using namespace std::chrono_literals;
-
-    const auto start = std::chrono::steady_clock::now();
-    while (true) {
-        auto now = std::chrono::steady_clock::now();
-        auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(now - start);
-        if (elapsed_seconds >= duration) {
-            break;
-        }
-    }
-
-    // call function
-    func();
-}
-
-void schedule(std::function<void(int)> func, std::chrono::seconds duration, int func_arg) {
+template <typename FUNC_PTR_T, typename... Targs>
+void schedule(FUNC_PTR_T func, std::chrono::seconds duration, Targs... args) {
     using namespace std::chrono_literals;
 
     const auto start = std::chrono::system_clock::now();
@@ -36,23 +17,5 @@ void schedule(std::function<void(int)> func, std::chrono::seconds duration, int 
     }
 
     // call function
-    func(func_arg);
-}
-
-// Napisz kolejne przeciążenie, które będzie przyjmować funkcje,
-// które przyjmują std::string oraz double jako swoje argumenty i nic nie zwracają.
-void schedule(std::function<void(std::string, double)> func, std::chrono::seconds duration, std::string text, double value) {
-    using namespace std::chrono_literals;
-
-    const auto start = std::chrono::system_clock::now();
-    while (true) {
-        auto now = std::chrono::system_clock::now();
-        auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(now - start);
-        if (elapsed_seconds >= duration) {
-            break;
-        }
-    }
-
-    // call function
-    func(text, value);
+    func(args...);
 }
