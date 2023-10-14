34b6cb37c3bb88d5af3aa7953728c30cce800054
Bartosz Rozenkop <bartez33a@gmail.com>
2023-10-14 20:46:31 +0200

    modified main.cpp - to original version (deleted definitions of schedule) schedule.hpp - now it defines schedule functions (not only declares)

--- homework/schedule/main.cpp
+++ homework/schedule/main.cpp
@@ -1,103 +1,30 @@
-/*
-   1. create function schedule(), which takes two parameters:
-   -  first - any function "func" which takes no parameter and returns nothing
-   -  second - "duration" - time period in secnods (use library std::chrono).
-   Function schedule should wait "duration" seconds and then call function
-   "func".
-
-   2. Create overload of the function "schedule", which takes two arguments:
-      -  any function which takes int and returns nothing, and
-      -  int (instead of "duration").
-      How to pass int argument to this function?
-
-   3. Create another overload of schedule, which takes functions which take
-      std::string and double as their parameters and returns nothing.
-
-   Bonus: implement schedule as a templete function. One version will be enough
-   (without any overloads)!
-
-*/
-
 #include <chrono>
-#include <functional>
 #include <iomanip>
 #include <iostream>
 #include <string>
+#include "schedule.hpp"
 
-/*
-   Inputs:
-   -  func     - pointer to function which takes no argument and reutrns nothing
-   -  duration - duration of time in seconds after which func is called
-*/
-void schedule(std::function<void(void)> func, const std::chrono::seconds duration) {
-    using namespace std::chrono_literals;
+// Napisz funkcję schedule(), która przyjmuje dwa parametry:
+//   Pierwszy parametr to dowolną funkcję func, która nie przyjmuje żadnego argumentu i nic nie zwraca
+//   Drugi parametr to okres czasu duration wyrażony w sekundach (użyj biblioteki std::chrono) Funkcja schedule() ma odczekać podany okres czasu duration, a potem wywołać funkcję func.
+// Napisz również przeciążenie funkcji schedule(), które oprócz okresu czasu ma przyjąć dowolną funkcję, która przyjmuje int i nic nie zwraca. Pomyśl jak przekazać ten argument int do tej funkcji.
+// Napisz kolejne przeciążenie, które będzie przyjmować funkcje, które przyjmują std::string oraz double jako swoje argumenty i nic nie zwracają.
+// Na gałęzi schedule masz już gotowy szkielet do tej aplikacji. Tam dostarcz swój kod.
 
-    const auto start = std::chrono::steady_clock::now();
-    while (true) {
-        auto now = std::chrono::steady_clock::now();
-        auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(now - start);
-        if (elapsed_seconds >= duration) {
-            break;
-        }
-    }
-
-    // call function
-    func();
-}
-
-void schedule(std::function<void(int)> func, const std::chrono::seconds duration, int func_arg) {
-    using namespace std::chrono_literals;
-
-    const auto start = std::chrono::system_clock::now();
-    while (true) {
-        auto now = std::chrono::system_clock::now();
-        auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(now - start);
-        if (elapsed_seconds >= duration) {
-            break;
-        }
-    }
-
-    // call function
-    func(func_arg);
-}
-
-// Napisz kolejne przeciążenie, które będzie przyjmować funkcje,
-// które przyjmują std::string oraz double jako swoje argumenty i nic nie zwracają.
-void schedule(std::function<void(std::string, double)> func, const std::chrono::seconds duration, std::string text, double value) {
-    using namespace std::chrono_literals;
-
-    const auto start = std::chrono::system_clock::now();
-    while (true) {
-        auto now = std::chrono::system_clock::now();
-        auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(now - start);
-        if (elapsed_seconds >= duration) {
-            break;
-        }
-    }
-
-    // call function
-    func(text, value);
-}
+// Bonus: zaimplementuj schedule jako funkcję szablonową. Wystarczy wtedy tylko jedna jej wersja, bez żadnych przeciążeń.
+// Uwaga, to trudne zadanie na tym etapie kursu!
 
 int main() {
     using namespace std::chrono_literals;
 
     auto start = std::chrono::system_clock::now();
-    std::cout << "start!\n";
     schedule([] { std::cout << "I'm an empty function\n"; }, 1s);
     schedule([](int a) { std::cout << "Param int a = " << a << '\n'; }, 2s, 42);
-
-    schedule(
-        [](std::string s, double d) {
-            std::cout << "Params: string s  = " << s << ", double d = " << d << '\n';
-        },
-        0s, "text", 42.5);
-
+    schedule([](std::string s, double d) { std::cout << "Params: string s = " << s << ", double d = " << d << '\n'; }, 0s, "text", 42.5);
     auto stop = std::chrono::system_clock::now();
 
     std::chrono::duration<double> diff = stop - start;
-    std::cout << "Everything took " << std::fixed << std::setprecision(6) << diff.count()
-              << " seconds\n";
+    std::cout << "Everything took " << std::fixed << std::setprecision(6) << diff.count() << " seconds\n";
 
     return 0;
 }
--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -3,6 +3,56 @@
 #include <functional>
 #include <string>
 
-void schedule(std::function<void(void)> func, const std::chrono::seconds duration);
-void schedule(std::function<void(int)> func, const std::chrono::seconds duration, int func_arg);
-void schedule(std::function<void(std::string, double)> func, const std::chrono::seconds duration, std::string text, double value);
+// void schedule(std::function<void(void)> func, std::chrono::seconds duration);
+// void schedule(std::function<void(int)> func, std::chrono::seconds duration, int func_arg);
+// void schedule(std::function<void(std::string, double)> func, std::chrono::seconds duration, std::string text, double value);
+
+void schedule(std::function<void(void)> func, std::chrono::seconds duration) {
+    using namespace std::chrono_literals;
+
+    const auto start = std::chrono::steady_clock::now();
+    while (true) {
+        auto now = std::chrono::steady_clock::now();
+        auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(now - start);
+        if (elapsed_seconds >= duration) {
+            break;
+        }
+    }
+
+    // call function
+    func();
+}
+
+void schedule(std::function<void(int)> func, std::chrono::seconds duration, int func_arg) {
+    using namespace std::chrono_literals;
+
+    const auto start = std::chrono::system_clock::now();
+    while (true) {
+        auto now = std::chrono::system_clock::now();
+        auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(now - start);
+        if (elapsed_seconds >= duration) {
+            break;
+        }
+    }
+
+    // call function
+    func(func_arg);
+}
+
+// Napisz kolejne przeciążenie, które będzie przyjmować funkcje,
+// które przyjmują std::string oraz double jako swoje argumenty i nic nie zwracają.
+void schedule(std::function<void(std::string, double)> func, std::chrono::seconds duration, std::string text, double value) {
+    using namespace std::chrono_literals;
+
+    const auto start = std::chrono::system_clock::now();
+    while (true) {
+        auto now = std::chrono::system_clock::now();
+        auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(now - start);
+        if (elapsed_seconds >= duration) {
+            break;
+        }
+    }
+
+    // call function
+    func(text, value);
+}
