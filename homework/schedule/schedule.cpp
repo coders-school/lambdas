876701815ca0aab19d909f9f6bca400716e945d2
Michał Maj <47927347+michalmaj@users.noreply.github.com>
2024-02-06 17:32:21 +0100

    Create schedule.cpp

---
+++ homework/schedule/schedule.cpp
@@ -0,0 +1,24 @@
+#include "schedule.hpp"
+
+void schedule(std::function<void()> func, std::chrono::seconds duration) {
+  auto start = std::chrono::steady_clock::now();
+
+  while (std::chrono::steady_clock::now() - start < duration);
+  func();
+}
+
+void schedule(std::function<void(int)> func, std::chrono::seconds duration, int val) {
+  auto start = std::chrono::steady_clock::now();
+
+  while (std::chrono::steady_clock::now() - start < duration)
+    ;
+  func(val);
+}
+
+void schedule(std::function<void(std::string, double)> func,
+              std::chrono::seconds duration, std::string str, double val) {
+  auto start =  std::chrono::steady_clock::now();
+
+  while (std::chrono::steady_clock::now() - start < duration);
+  func(str, val);
+}
