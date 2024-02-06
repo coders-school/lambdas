48bc161849f9bd8732a8b332d8d03a18b1276935
Michał Maj <47927347+michalmaj@users.noreply.github.com>
2024-02-06 17:48:30 +0100

    Update schedule.cpp

--- homework/schedule/schedule.cpp
+++ homework/schedule/schedule.cpp
@@ -1,26 +1,25 @@
 #include "schedule.hpp"
 
 void schedule(std::function<void()> func, std::chrono::seconds duration) {
-  auto start = std::chrono::steady_clock::now();
+    auto start = std::chrono::steady_clock::now();
 
-  while (std::chrono::steady_clock::now() - start < duration)
-    ;
-  func();
+    while (std::chrono::steady_clock::now() - start < duration)
+        ;
+    func();
 }
 
 void schedule(std::function<void(int)> func, std::chrono::seconds duration, int val) {
-  auto start = std::chrono::steady_clock::now();
+    auto start = std::chrono::steady_clock::now();
 
-  while (std::chrono::steady_clock::now() - start < duration)
-    ;
-  func(val);
+    while (std::chrono::steady_clock::now() - start < duration)
+        ;
+    func(val);
 }
 
-void schedule(std::function<void(std::string, double)> func,
-              std::chrono::seconds duration, std::string str, double val) {
-  auto start =  std::chrono::steady_clock::now();
+void schedule(std::function<void(std::string, double)> func, std::chrono::seconds duration, std::string str, double val) {
+    auto start =  std::chrono::steady_clock::now();
 
-  while (std::chrono::steady_clock::now() - start < duration)
-    ;
-  func(str, val);
+    while (std::chrono::steady_clock::now() - start < duration)
+        ;
+    func(str, val);
 }
